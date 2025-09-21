6e710f7bf2820aa898e31f1b48bd2c11df89f56e
SERGIO-ANDRES-CASTRO-VELASQUEZ <sergio_castrove@fet.edu.co>
2025-09-21 16:36:33 -0500

    Creacion y manejo de Excepciones

---
+++ src/main/java/com/trabajo/gestiontareas/exception/ResponseExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.trabajo.gestiontareas.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> ManejarEntityNotFoundException(EntityNotFoundException ex, WebRequest request){
+        ExceptionResponse error = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
