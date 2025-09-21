31a56ddcb09be4ea7a000b16151d72e27b37b5a2
Ninjaky22 <juan_restrepope@fet.edu.co>
2025-09-21 17:49:52 -0500

    Realizacion Capa Services

--- src/main/java/com/trabajo/gestiontareas/service/TareaService.java
+++ src/main/java/com/trabajo/gestiontareas/service/TareaService.java
@@ -1,4 +1,57 @@
 package com.trabajo.gestiontareas.service;
 
+import com.trabajo.gestiontareas.entity.Tarea;
+import com.trabajo.gestiontareas.repository.TareaRepo;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TareaService {
+    @Autowired
+    private TareaRepo repo;
+
+    public List<Tarea> listarTareas() {
+        return repo.findAll();
+    }
+
+    public Tarea crearTarea (Tarea tarea) {
+        return repo.save(tarea);
+    }
+
+    public Tarea actualizarTarea(Long id, Tarea tareaActualizada){
+        Optional<Tarea> tarea = repo.findById(id);
+
+
+        if (tarea.isPresent()) {
+            Tarea tareaDb = tarea.get();
+            tareaDb.setTitulo(tareaActualizada.getTitulo());
+            tareaDb.setDescripcion(tareaActualizada.getDescripcion());
+            tareaDb.setFechaVencimiento(tareaActualizada.getFechaVencimiento());
+            tareaDb.setPrioridad(tareaActualizada.getPrioridad());
+            tareaDb.setEstado(tareaActualizada.getEstado());
+            return repo.save(tareaDb);
+        } else {
+            throw new EntityNotFoundException("Tarea no encontrada para actualizar.");
+        }
+    }
+
+    public Tarea obtenerTareaPorId(Long id) {
+        Tarea tarea = repo.findById(id).orElse(null);
+        if (tarea == null) {
+            throw new EntityNotFoundException("Id no encontrado " + id);
+        }
+        return tarea;
+    }
+
+    public void eliminarTarea(Long id) {
+        if (!repo.existsById(id)) {
+            throw new EntityNotFoundException("Tarea no encontrada para eliminar.");
+        }
+        repo.deleteById(id);
+    }
 }
