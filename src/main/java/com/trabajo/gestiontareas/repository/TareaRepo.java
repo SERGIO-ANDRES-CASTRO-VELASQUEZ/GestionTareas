904197c40eebce9c7ea2a2e8ea155c3275d588b7
CasanovaCristian <cristian_casanovaan@fet.edu.co>
2025-09-21 19:54:19 -0500

    Se añadió los metodos personalizados para filtrar

--- src/main/java/com/trabajo/gestiontareas/repository/TareaRepo.java
+++ src/main/java/com/trabajo/gestiontareas/repository/TareaRepo.java
@@ -1,7 +1,13 @@
 package com.trabajo.gestiontareas.repository;
 
 import com.trabajo.gestiontareas.entity.Tarea;
+import com.trabajo.gestiontareas.entity.enums.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TareaRepo extends JpaRepository<Tarea, Long> {
+    List<Tarea> findByEstado(Estado estado);
+    List<Tarea> findByFechaVencimientoBefore(LocalDate fecha);
 }
